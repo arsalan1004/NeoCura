52dbd0319efb2a3b99c52c939318f6f8479fa27b
Syed Arsalan <syedarsalan3505@gmail.com>
2023-07-15 22:40:33 +0500

    Completed Doctor List API

--- backend/src/api/controllers/indv_doc_controller/get_functions/Location_Info/OnlineConsultation/Details/get_consult_details.js
+++ backend/src/api/controllers/indv_doc_controller/get_functions/Location_Info/OnlineConsultation/Details/get_consult_details.js
@@ -1,13 +1,15 @@
 const { getTimetable } = require("./Timetable/get_Timetable.js");
 const { getConsultId } = require("./Id/get_consult_id.js");
+const { getFees } = require("./Fees/get_fees.js");
 
 const getDetails = async (docId) => {
   const consultId = await getConsultId(docId);
-  console.log(consultId);
+  const feeAmount = await getFees(consultId, docId);
   const timetable = await getTimetable(consultId, docId);
 
   const result = {
     name: "Online Video Consultation",
+    fees: feeAmount,
     timetable: timetable[0],
   };
 
