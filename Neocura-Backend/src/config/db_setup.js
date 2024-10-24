95c3ced0fcacaa5fbee891d18acda079e36da2c2
Arsalan <syed.arsalan@transviti.com>
2024-10-24 10:38:44 +0500

    env setup

--- Neocura-Backend/src/config/db_setup.js
+++ Neocura-Backend/src/config/db_setup.js
@@ -1,11 +1,12 @@
 const { Client } = require("pg");
+require("dotenv").config();
 
 const database = new Client({
   host: "localhost",
   user: "postgres",
   port: 5432,
-  password: "arsalan",
-  database: "NeoCura",
+  password: `${process.env.PASSWORD}`,
+  database: `${process.env.DATABASE}`,
 });
 
 database.connect((err) => {
